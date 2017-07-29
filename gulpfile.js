7bc904d2a26caf407620c51f0819ccadd2f654db
charleyblue <charley.blewett@gmail.com>
2017-07-29 16:19:49 -0700

    Added net gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,21 @@ var gulp = require('gulp'),
 watch = require('gulp-watch');
 
 gulp.task('default', logOut);
+gulp.task('html', updateHTML);
+gulp.task('watch', watchHTML );
 
 function logOut() {
   console.log("First Gulp task wrote this in the console. Oh boy!");
+}
+
+function updateHTML() {
+  console.log("We'll just console this message for now");
+}
+
+function watchHTML() {
+  watch('./app/index.html', startHTML);
+}
+
+function startHTML(){
+  gulp.start('html');
 }

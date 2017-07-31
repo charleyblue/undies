bfd7fbddbec06a5b877cdedf13cf024beeead073
charleyblue <charley.blewett@gmail.com>
2017-07-30 18:10:31 -0700

    Added postcss and css processor to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoPrefix = require('autoprefixer'),
+cssVars = require('postcss-simple-vars')
+nested = require('postcss-nested');
 
 gulp.task('default', logOut);
 gulp.task('html', updateHTML);
-gulp.task('watch', watchHTML );
+gulp.task('styles', updateCSS);
+gulp.task('watch', watchApp);
 
 function logOut() {
   console.log("First Gulp task wrote this in the console. Oh boy!");
@@ -13,10 +18,20 @@ function updateHTML() {
   console.log("We'll just console this message for now");
 }
 
-function watchHTML() {
+function updateCSS() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([autoPrefix, cssVars, nested]))
+    .pipe(gulp.dest('./app/temp/styles'));
+}
+
+function watchApp() {
   watch('./app/index.html', startHTML);
+  watch('./app/assets/styles/**/*.css', startCSS);
 }
 
 function startHTML(){
   gulp.start('html');
 }
+ function startCSS() {
+   gulp.start('styles');
+ }
